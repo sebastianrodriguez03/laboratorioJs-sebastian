3a456fca073d155d0941a29837e05250ae257771
sebastianrodriguez03 <sebastianrodriguez0303@gmail.com>
2018-12-07 10:46:05 -0500

    Initial commit

---
+++ js/ejercicio01.js
@@ -0,0 +1,4 @@
+// var con=prompt("Valor consignado");
+// document.write("<h1>Valor consigando</h1>"+"<br>"+con);
+// var high=con*Math.pow(1+0.02,60);
+// document.write("<h1>Vaolor futuro de 5 años con un interes del 2%</h1>"+"<br>"+high);
---
+++ js/ejercicio2.js
@@ -0,0 +1,28 @@
+var total;
+var total1;
+var descuento;
+var totalDescuento;
+var kilo=parseInt(prompt("Ingrese el numero de kilos "));
+total=(kilo)*(4200);
+alert("Su compra es de "+ kilo +"  kilo(s) El costo es "+total);
+
+if(kilo >2,01 && kilo <5){
+    doscuento=(kilo)*(0,10);
+    totalDescuento=(total)-(descuento);
+    alert( "su descuento es de" +totalDescuento );
+}
+if(kilo >5,01 && kilo <10){
+    doscuento=(kilo)*(0,15);
+    totalDescuento=(total)-(descuento);
+    alert( "su descuento es de" +totalDescuento );
+}
+if(kilo >10,01){
+    doscuento=(kilo)*(0,20);
+    totalDescuento=(total)-(descuento);
+    alert( "su descuento es de" +totalDescuento );
+}
+
+
+
+
+
